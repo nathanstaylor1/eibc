a8a31aef9d553baa9535db858180bb80d31068a4
NathanMBP <nathanstaylor1@gmail.com>
2015-08-17 22:27:34 +1000

    update to secure links

--- global/scripts.php
+++ global/scripts.php
@@ -1,4 +1,4 @@
-    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
     <script type='text/javascript' src='assets/js/jquery-ui.js'></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="assets/js/navbar-scroll.js"></script>
